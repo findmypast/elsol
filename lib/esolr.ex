ef17f22a6c967a7b1008cb5877f360cee616bf9a
Andy Mendelsohn <amendelsohn@mac.com>
2015-10-03 17:56:45 +0100

    lots of messing around trying to create a parser, but lots of missing options to deal with, therefore, for spike reasons, adding a query method to just pass through a fully formed URL

--- lib/esolr.ex
+++ lib/esolr.ex
@@ -2,60 +2,96 @@ defmodule Esolr do
 
   use Jazz
   use HTTPoison.Base
-#"/solr/select?q=*:*&rows=1000&fl=name&wt=json"
 
-  defstruct query_string: "", facets: [], wanted_fields: "", filters: [], start: 0, rows: 0, date: [], wt: "json"
+  defstruct url: "url", root: "solr", data_set: "testing", site: "", shards: "", query_string: "", facets: [], wanted_fields: "", filters: [], start: 0, rows: 0, date: [], wt: "json"
+
+  # When we just want to pass through a whole url and query_string...(no construction of url required)
+  def query(solr_query_url) when is_binary(solr_query_url) do
+    get(solr_query_url)
+  end
 
   def query(query_struct) do
-    solr_query_url = FigaroElixir.env["url"] <> "/solr/testing/select?" <> create_query(query_struct)
-    {:ok, json} = HTTPoison.get(solr_query_url)
-    reply = JSON.decode!(json.body)
-    response = reply["response"]
-    response["docs"]
+    solr_query_url = build_url_with_query(query_struct)
+    get(solr_query_url)
   end
 
+  def build_url_with_query(query_struct) do
+    FigaroElixir.env[query_struct.url] <> "/#{query_struct.root}/#{query_struct.data_set}/select?" <> create_query(query_struct)
+  end
+
+  def process_response_body(body) do
+    reply = JSON.decode!(body)
+  end
 
   def create_query( query_struct) do
-    query_request(query_struct) <> "&wt=json"
-#    fields(query_struct[:wanted_fields]) <>
-#    facet_fields(query_struct[:facets]) <>
-#    filter_queries(query_struct[:filters]) <>
-#    date(query_struct[:date]) <>
-#    start(query_struct[:start]) <>
-#    rows(query_struct[:rows])
+    query_request(query_struct) <>
+    fields(query_struct) <>
+    facets(query_struct) <>
+    filter_queries(query_struct) <>
+    date(query_struct) <>
+    start(query_struct) <>
+    rows(query_struct) <>
+    wt(query_struct)
   end
 
-
-  def query_request(%Esolr{query_string: query_string}) do
-    "q=#{query_string}"
-  end
-
-  def facet_fields(fields) do
-    "facet=true&facet_field=" <> Enum.join(fields,"&facet.field=")
-  end
-
-  def fields(field_names_string) do
-    "&fl=#{field_names_string}"
-  end
-
-  def filter_queries(filter_query_list) do
-    "&fq=" <> Enum.join(filter_query_list)
-  end
-
-
-  def start(start_number) do
-    "&start=#{start_number}"
-  end
-
-  def rows(row_count) do
-    "&rows=#{row_count}"
-  end
-
-  def date([date_label, date_start, date_end, gap]) do
+  def date(%Esolr{date: [date_label, date_start, date_end, gap]}) do
     "&facet.date=#{date_label}" <>
     "&f.#{date_label}.facet.range.start=#{date_start}" <>
     "&f.#{date_label}.facet.range.end=#{date_end}" <>
     "&f.#{date_label}.facet.range.gap=#{gap}"
   end
+
+  def date(%Esolr{date: []}) do
+    ""
+  end
+
+  def query_request(%Esolr{query_string: query_string}) do
+    "q=#{query_string}"
+  end
+
+  def facets(%Esolr{facets: []}) do
+    ""
+  end
+
+  def facets(%Esolr{facets: fields}) do
+    "facet=true&facet_field=" <> Enum.join(fields,"&facet.field=")
+  end
+
+  def fields(%Esolr{wanted_fields: ""}) do
+    ""
+  end
+
+  def fields(%Esolr{wanted_fields: wanted_fields}) do
+    "&fl=#{wanted_fields}"
+  end
+
+  def site(%Esolr{site: ""}) do
+    ""
+  end
+
+  def site(%Esolr{site: site_string}) do
+    "&site=#{site_string}"
+  end
+
+  def filter_queries(%Esolr{filters: []}) do
+    ""
+  end
+
+  def filter_queries(%Esolr{filters: filters}) do
+    "&fq=" <> Enum.join(filters,"&fq=")
+  end
+
+  def start(%Esolr{start: startnum}) do
+    "&start=#{startnum}"
+  end
+
+  def rows(%Esolr{rows: rowcount}) do
+    "&rows=#{rowcount}"
+  end
+
+  def wt(%Esolr{wt: wtype}) do
+    "&wt=#{wtype}"
+  end
+
 
 end
