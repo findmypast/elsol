ebd1032d3a13a1435562cbe04fc5ceb762e8ac2f
Boon Low <boon.low@mac.com>
2015-12-31 14:18:15 +0000

    support Terms component query

--- lib/elsol/query.ex
+++ lib/elsol/query.ex
@@ -37,6 +37,12 @@ defmodule Elsol.Query do
                                    suggest_build: nil, suggest_count: nil, suggest_cfq: nil, suggest_build: nil,
                                    suggest_reload: nil, suggest_buildAll: nil, suggest_reloadAll: nil
 
+  defmodule Terms, do: defstruct url: nil, name: "/terms", terms: true,  terms_fl: nil,
+                                 terms_limit: nil, terms_lower: nil, terms_lower_incl: nil, terms_mincount: nil,
+                                 terms_maxcount: nil, terms_prefix: nil, terms_raw: nil,
+                                 terms_regex: nil, terms_regex_flag: nil, terms_sort: nil,
+                                 terms_upper: nil, terms_upper_incl: nil
+
   def build(params) when is_map(params) do
     cond do
       Map.has_key?(params, :name) -> params.name <> "?" <> build( Map.delete(params, :url) |> Map.delete(:name) |> Map.to_list )
