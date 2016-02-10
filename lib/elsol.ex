27a113c4dc23379ceda0885553ef1b8139c9fe8b
Boon Low <boon.low@mac.com>
2016-02-10 15:54:13 +0000

    tidying up

--- lib/elsol.ex
+++ lib/elsol.ex
@@ -3,19 +3,10 @@ defmodule Elsol do
   use HTTPoison.Base
 
   # When we just want to pass through a whole url and query_string...(no construction of url required)
-  def query(solr_query_url) when is_binary(solr_query_url) do
-    get(solr_query_url, [],[recv_timeout: 30000])
-  end
+  def query(solr_query_url) when is_binary(solr_query_url), do: get solr_query_url, [], [recv_timeout: 30000]
 
-  def query(query_struct) do
-    solr_query_url = build_query(query_struct)
-    get(solr_query_url, [],[recv_timeout: 30000])
-  end
-
-  def query!(query_struct) do
-    solr_query_url = build_query(query_struct)
-    get!(solr_query_url, [],[recv_timeout: 30000])
-  end
+  def query(query_struct), do: get build_query(query_struct), [], [recv_timeout: 30000]
+  def query!(query_struct), do: get! build_query(query_struct), [], [recv_timeout: 30000]
 
   @doc """
   Build solr query with `%Elsol.Query{}` structs. See `Elsol.Query` for more details.
