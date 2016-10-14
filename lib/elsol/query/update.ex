61bf541931cc7b91ffb33bf8599285d76915cd36
Robert Elwell <robert.elwell@lookout.com>
2016-10-13 20:17:15 -0700

    got query working

--- lib/elsol/query/update.ex
+++ lib/elsol/query/update.ex
@@ -2,6 +2,6 @@ defmodule Elsol.Query.Update do
 
   defstruct url: nil, name: "/update", commit: nil, optimize: nil, 
             waitFlush: nil, waitSearcher: nil, expungeDeletes: nil,
-            maxSegments: nil, rollback: nil
+            maxSegments: nil, rollback: nil, collection: nil
 
 end
--- spec/lib/elsol_spec.exs
+++ spec/lib/elsol_spec.exs
@@ -1,12 +1,77 @@
 defmodule ElsolSpec do
 
-  use ESpec, async: true
+  use ESpec
+  
+  defmodule SharedQueryExample do
+  
+    use ESpec, shared: true
+
+    let_overridable [:method, :request_method]
+
+    subject do: apply(Elsol, method(), [arg()])
+
+    before do
+      allow Elsol |> to(accept request_method(), fn(_,_,_) -> "retval" end)
+    end
+
+    let :expected_url, do: "http://localhost:8983/solr/foo/select?q=*:*&wt=json"
+
+    context "with a string" do
+      let :arg, do: expected_url()
+
+      it "should request the URL through HTTPoison" do
+        should eql "retval"  # indicates we returned the return value of :get
+        expect Elsol |> to(accepted request_method(), [arg(), [], [recv_timeout: 30000]])
+      end
+    end
+
+    context "with a query struct" do
+      let :arg, do: %Elsol.Query{url: "http://localhost:8983/solr",
+                                 collection: %Elsol.Collection{name: "foo"},
+                                 q: "*:*"}
+      it "should request from the correct URL" do
+        should eql "retval"
+        expect Elsol |> to(accepted request_method(), [expected_url(), [], [recv_timeout: 30000]])
+      end
+    end
+  end
+  
+#  defmodule SharedUpdateExample do
+#
+#    use ESpec, shared: true
+#    let_overridable [:method, :request_method]
+#
+#    subject do: apply(Elsol, method(), args())
+#
+#  end
 
   example_group do
     describe "query" do
-      it "should query Solr with the query string provided and return a result" do
-        true
-      end
+      let :method, do: :query
+      let :request_method, do: :get
+      it_behaves_like SharedQueryExample
     end
+    
+    describe "query!" do
+      let :method, do: :query!
+      let :request_method, do: :get!
+      it_behaves_like SharedQueryExample
+    end
+#
+#    describe "update" do
+#
+#      let :struct, do: %Elsol.Query.Update{commit: true, collection: %Elsol.Collection{name: "foo"}}
+#
+#      context "with docs" do
+#
+#      end
+#
+#      context "without docs" do
+#
+#      end
+#
+#    end
+    
+    
   end
 end
