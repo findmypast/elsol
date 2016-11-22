2b4b742dc63e7ca233ee849bf8af4a098c6aba36
Boon Low <boon.low@mac.com>
2016-11-22 16:28:08 +0000

    support hl.snippets

--- lib/elsol/query/highlight.ex
+++ lib/elsol/query/highlight.ex
@@ -1,5 +1,5 @@
 defmodule Elsol.Query.Highlight do
-  defstruct hl: true, hl_fl: nil, hl_fragsize: nil,
+  defstruct hl: true, hl_fl: nil, hl_fragsize: nil, hl_snippets: nil,
             hl_requireFieldMatch: nil, hl_tag_pre: nil,
             hl_tag_post: nil, hl_useFastVectorHighlighter: nil
 end
