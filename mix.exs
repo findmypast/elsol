f6606072ce47e0f7fc4c73e7c5426a5036390756
Justin Coyne <jcoyne@justincoyne.com>
2017-03-04 22:50:11 -0600

    Add parenthesis to make deps an unabmiguous function call. Fixes #9

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Elsol.Mixfile do
      spec_pattern: "*_spec.exs",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   # Configuration for the OTP application
