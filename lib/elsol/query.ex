e82cad3bacd6982a9acf8a43ccb925a8eb07f875
Philip McGavin <pdsmcgavin@gmail.com>
2020-10-06 16:12:18 +0100

    DENG-000: Adding default of no identing so that the json is parsed correctly

--- lib/elsol/query.ex
+++ lib/elsol/query.ex
@@ -1,11 +1,10 @@
 defmodule Elsol.Query do
-
   @moduledoc """
     Example of a simple and extensible Elixir module for building Solr query string
-    according to existing syntax, using a single `build` function type with 
+    according to existing syntax, using a single `build` function type with
     pattern-matching and recursion.
 
-    For sub-parameters (dotted) such as `facet.count`, `facet.range.gap`, substitute `.` 
+    For sub-parameters (dotted) such as `facet.count`, `facet.range.gap`, substitute `.`
     with `_` in field keys (atoms), e.g. `facet_count`, `facet_range_gap`.
 
     Note that because of the nature of how Elixir handles maps and lists,
@@ -18,48 +17,65 @@ defmodule Elsol.Query do
 
         iex> Elsol.Query.build(%Elsol.Query{q: "market", rows: 20, fq: ["postcode:1234", "city:london"], fl: "*"})
         "/select?fl=*&fq=postcode:1234&fq=city:london&q=market&rows=20"
-    
+
     ## use the `%Elsol.Query{}` and `%Elsol.Query.Facet` structs to include faceting
-        
+
         iex> Elsol.Query.build( Map.merge %Elsol.Query{q: "{!lucene}*:*"}, %Elsol.Query.Facet{facet_field: ["postcode", "city"]})
         "/select?facet=true&facet.field=postcode&facet.field=city&q={!lucene}*:*"
 
   """
 
-  defstruct url: nil, name: "/select", q: nil, fq: nil, start: nil, rows: nil,
-            wt: "json", tr: nil, sort: nil, echoParams: nil, fl: nil, collection: %Elsol.Collection{}
+  defstruct url: nil,
+            name: "/select",
+            q: nil,
+            fq: nil,
+            start: nil,
+            rows: nil,
+            indent: "off",
+            wt: "json",
+            tr: nil,
+            sort: nil,
+            echoParams: nil,
+            fl: nil,
+            collection: %Elsol.Collection{}
 
   def build(params) when is_map(params) do
     cond do
       Map.has_key?(params, :name) ->
-        Elsol.Collection.path(params.collection) # this can be an empty string or /collection_name
-        <> params.name
-        <> "?"
-        <> build( Map.drop(params, [:url, :name]) |> Map.to_list )  # query params
-      true -> build( Map.to_list(params) )
+        # this can be an empty string or /collection_name
+        # query params
+        Elsol.Collection.path(params.collection) <>
+          params.name <>
+          "?" <>
+          build(Map.drop(params, [:url, :name]) |> Map.to_list())
+
+      true ->
+        build(Map.to_list(params))
     end
   end
 
-  def build([head|tail]) do
+  def build([head | tail]) do
     (build(head) <> build(tail)) |> String.rstrip(?&)
   end
 
-  def build({k,v}) when is_bitstring(v) or is_integer(v) do
-    (Atom.to_string(k) |> String.replace("_","."))  <> "=#{v}&"
+  def build({k, v}) when is_bitstring(v) or is_integer(v) do
+    (Atom.to_string(k) |> String.replace("_", ".")) <> "=#{v}&"
   end
 
-  def build({k, v}) when is_boolean(v), do: (Atom.to_string(k) |> String.replace("_",".")) <> "=#{v}&"
+  def build({k, v}) when is_boolean(v),
+    do: (Atom.to_string(k) |> String.replace("_", ".")) <> "=#{v}&"
 
-  def build({k,v}) when is_list(v) and length(v)>0 do
+  def build({k, v}) when is_list(v) and length(v) > 0 do
     build({k, hd(v)}) <> build({k, tl(v)})
   end
 
-  def build({:collection, %Elsol.Collection{query: query} = _collection}) when is_bitstring(query), do: "collection=#{query}&"
+  def build({:collection, %Elsol.Collection{query: query} = _collection})
+      when is_bitstring(query),
+      do: "collection=#{query}&"
 
   def build({:collection, _collection}), do: ""
   def build({:__struct__, _}), do: ""
   def build({_, nil}), do: ""
-  def build({_,[]}), do: ""
+  def build({_, []}), do: ""
   def build([]), do: ""
-
 end
